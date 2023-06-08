3ec31cbb242f1f5af8d544708c949e5fdb6ebdd3
largenumberhere <largeNumbergoeshere@gmail.com>
2023-06-08 18:16:34 +1000

    Added inline documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,45 @@
+#![warn(missing_docs)]
+#![warn(missing_doc_code_examples)]
+//! A small library with some string helpers
 use std::fmt::Write;
 
-pub trait WriteStringExt{
+///Writes a str to a string.
+///
+/// Example:
+/// ```
+/// let mut string = String::new();
+/// string.write("hello world!");
+/// assert_eq!("hello world!".to_string(), string);
+/// ```
+pub trait WriteStringExt
+{
+    ///Please see trait documentation
     fn write(&mut self, str:&str) -> &mut Self;
 }
 
+
 impl WriteStringExt for String{
-    ///Writes a str to a string.
-    /// It is a very thin wrapper for the write! macro so it can be appended to strings
     fn write(&mut self, str: &str) ->&mut  String {
         write!(self, "{}", str).expect("failed to push to string!");
         self
     }
 }
-
+///Writes a str and \n to a string.
+/// It is a very thin wrapper for the writeln! macro so it can be appended to strings
+///
+/// Example:
+///```
+/// let mut string = String::new();
+/// string.writeln("hello world!");
+/// assert_eq!("hello world!\n".to_string(), string);
+/// ```
 pub trait WriteLnStringExt{
+    ///Please see trait documentation
     fn writeln(&mut self, str:&str) -> &mut Self;
 }
 
 impl WriteLnStringExt for String{
-    ///Writes a str and \n to a string.
-    /// It is a very thin wrapper for the writeln! macro so it can be appended to strings
+
     fn writeln(&mut self, str: &str) ->&mut  String {
         writeln!(self, "{}", str).expect("failed to push to string!");
         self
